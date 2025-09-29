98a5f3282ff9d9e61a8956b4c7110eb67009b715
0xjei <hello@0xjei.dev>
2025-09-29 14:23:57 +0200

    add main template generator

--- crates/circuits/greco/src/lib.rs
+++ crates/circuits/greco/src/lib.rs
@@ -7,5 +7,6 @@
 pub mod bounds;
 pub mod circuit;
 pub mod sample;
+pub mod template;
 pub mod toml;
 pub mod vectors;
--- crates/circuits/pk_pvw/src/lib.rs
+++ crates/circuits/pk_pvw/src/lib.rs
@@ -15,5 +15,6 @@
 pub mod bounds;
 pub mod circuit;
 pub mod sample;
+pub mod template;
 pub mod toml;
 pub mod vectors;
